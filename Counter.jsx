4ba7ab910338ea53f763e21fe8a6ac24c07e8e61
Harwina <164463440+Harwina@users.noreply.github.com>
2024-09-21 21:35:55 +0530

    Update Counter.jsx

--- Counter.jsx
+++ Counter.jsx
@@ -1,56 +1,13 @@
-import { Component } from "react";
+const Counter = ({ count, increment, decrement, reset }) => {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={increment} className="btn btn-success me-3">+</button>
+      <button onClick={decrement} className="btn btn-danger me-3" disabled={count === 0}>-</button>
+      <button onClick={reset} className="btn btn-warning">Reset</button>
+    </div>
+  );
+};
 
-
-class Count extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cnt: 0,
-    };
-  }
-  plus = () => {
-    this.setState({
-      cnt: this.state.cnt + 1,
-    });
-  };
-
-  dec = () => {
-    this.setState({
-      cnt: this.state.cnt - 1,
-    });
-  };
-  reset = () => {
-    this.setState({
-      cnt: this.state.cnt
-    });
-  };
-  render() {
-    return (
-      <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-        <div className="card shadow-sm p-4" style={{ width: "350px", borderRadius: "15px" }}>
-          <div className="card-body text-center">
-            <h1 className="mb-4 text-primary fw-bold">Counter</h1>
-            <p className="fw-bold fs-1">{this.state.cnt}</p>
-            <div className="d-flex justify-content-between">
-              <button className="btn btn-success btn-lg px-4" onClick={this.plus}>
-                +
-              </button>
-              {this.state.cnt <= 0 ? (
-                <button disabled className="btn btn-danger btn-lg px-4" onClick={this.dec}>
-                  -
-                </button>
-              ) : (
-                <button className="btn btn-danger btn-lg px-4" onClick={this.dec}>
-                  -
-                </button>
-              )}
-            </div>
-            <button className="btn btn-primary mt-5" onClick={this.reset}>reset</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Count;
+export default Counter;
